e1668b0e79d97b7fdc8ddb554606296747186745
Wenxin Yang <wxyang.wenxin@gmail.com>
2023-12-08 10:28:09 -0800

    remove previous files

--- prev/scripts/metrics/landscapmetrics.R
+++
@@ -1,9 +0,0 @@
-library(landscapemetrics)
-library(landscapetools)
-show_landscape(landscape, discrete = TRUE)
-
-# 1. Euclidean nearest neighbor
-lsm_p_enn(landscape)
-
-options_landscapemetrics(to_disk = NULL)
-
